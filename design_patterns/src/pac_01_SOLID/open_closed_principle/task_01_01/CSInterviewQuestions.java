7002a1b853faa0d845b29dec85719f763666d386
Dariusz Okoński <darek200180@gmail.com>
2023-01-12 21:10:47 +0100

    add CSInterviewQuestins class

---
+++ design_patterns/src/pac_01_SOLID/open_closed_principle/task_01_01/CSInterviewQuestions.java
@@ -0,0 +1,8 @@
+package pac_01_SOLID.open_closed_principle.task_01_01;
+
+public class CSInterviewQuestions implements InterviewQuestion {
+    @Override
+    public void execute() {
+        System.out.println("CS related questions...");
+    }
+}
