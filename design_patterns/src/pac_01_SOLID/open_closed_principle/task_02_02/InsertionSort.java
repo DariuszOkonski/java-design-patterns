26fee095eb73e4e8374180c360ff94d9e14efd38
Dariusz Okoński <darek200180@gmail.com>
2023-01-12 21:20:27 +0100

    add task 2 to open closed principle

---
+++ design_patterns/src/pac_01_SOLID/open_closed_principle/task_02_02/InsertionSort.java
@@ -0,0 +1,9 @@
+package pac_01_SOLID.open_closed_principle.task_02_02;
+
+public class InsertionSort implements SortingAlgorithm{
+    @Override
+    public void sort() {
+        System.out.println("Insertion sort is sorting the items...");
+    }
+}
+
