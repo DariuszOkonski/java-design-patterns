a6f3987b4fef1a81c494fcf7a70d8fd6818fde0f
Dariusz Okoński <darek200180@gmail.com>
2023-01-31 18:51:30 +0100

    add integer reversion

---
+++ design_patterns/src/_02_DataStructures_01/_01_Arrays/Test_04/App.java
@@ -0,0 +1,11 @@
+package _02_DataStructures_01._01_Arrays.Test_04;
+
+public class App {
+    public static void main(String[] args) {
+
+        IntegerReversion problem = new IntegerReversion();
+        System.out.println(problem.reverse(1234));
+        System.out.println(problem.reverse(1372783));
+
+    }
+}
