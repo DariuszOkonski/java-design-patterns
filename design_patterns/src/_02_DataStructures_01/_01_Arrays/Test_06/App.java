50763b6f6c00064829466f3f8c37096fecfa7f55
Dariusz Okoński <darek200180@gmail.com>
2023-02-07 18:51:20 +0100

    add test 06

---
+++ design_patterns/src/_02_DataStructures_01/_01_Arrays/Test_06/App.java
@@ -0,0 +1,8 @@
+package _02_DataStructures_01._01_Arrays.Test_06;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ design_patterns/src/_02_DataStructures_01/_01_Arrays/Test_06/TrappingRainWaterProblem.java
@@ -0,0 +1,5 @@
+package _02_DataStructures_01._01_Arrays.Test_06;
+
+public class TrappingRainWaterProblem {
+
+}
