b5dcd3b793407e53195dec8882f9c3def2401bf7
Dariusz Okoński <darek200180@gmail.com>
2023-02-07 18:05:36 +0100

    add builder pattern

---
+++ design_patterns/src/_01_DesignPatterns/pac_02_creational_design_patterns/BuilderPattern/App.java
@@ -0,0 +1,7 @@
+package _01_DesignPatterns.pac_02_creational_design_patterns.BuilderPattern;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("BuilderPattern");
+    }
+}
